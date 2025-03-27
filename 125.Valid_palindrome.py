ce61b85f268b340ca78cb20c24555ac3d69e37c7
Aneerudh <paneerudh@gmail.com>
2025-03-27 19:29:55 +0530

    initial commit

---
+++ 125.Valid_palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        left = 0
+        right = len(s) - 1
+
+        while left < right :
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -=1
+        return True
+
+#alt easier solution without using 2 pointers
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        return s == s[::-1]
