dd91e18fbcde25130ee50d7f72df240af0f533f3
Aneerudh <paneerudh@gmail.com>
2025-04-13 14:39:32 +0530

    update

--- 74.Search_2d_matrix.py
+++ 74.Search_2d_matrix.py
@@ -6,3 +6,22 @@ class Solution:
                     if i == target:
                         return True
         return False
+    
+'''
+optional rust solution
+impl Solution {
+    pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+        for row in matrix {
+            if row.is_empty() {
+                continue;
+            }
+
+            let last = row[row.len() - 1];
+            if last >= target {
+                return row.binary_search(&target).is_ok();
+            }
+        }
+        false
+    }
+}
+'''
