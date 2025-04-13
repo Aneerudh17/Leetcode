30b19d7c3f12a634a84fbc93f7b2842c07d16651
Aneerudh <paneerudh@gmail.com>
2025-04-13 14:38:58 +0530

    initial commit

---
+++ 74.Search_2d_matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if row[-1] >= target:
+                for i in row:
+                    if i == target:
+                        return True
+        return False
