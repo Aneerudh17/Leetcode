299f2f8e00ef2e2f85311f5bbfa65ea948ba1ef3
Aneerudh <paneerudh@gmail.com>
2025-04-14 15:53:12 +0530

    update

--- 1492.Kth_factor_of_n.py
+++ 1492.Kth_factor_of_n.py
@@ -9,3 +9,24 @@ class Solution:
             return factor[k - 1]
         else:
             return -1
+        
+'''
+optional rust solution:
+impl Solution {
+    pub fn kth_factor(n: i32, k: i32) -> i32 {
+        let mut factor = Vec::new();
+        for i in 1..=n{
+            if n % i == 0{
+                factor.push(i);
+            }
+        }
+        if (k as usize) <= factor.len(){
+            return factor[(k-1)as usize];
+        }
+        else{
+            return -1;
+        }
+        
+    }
+}
+'''
