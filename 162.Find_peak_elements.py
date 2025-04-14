8b74b7b2c5c66dab2df9844a8a2142c9ddb47338
Aneerudh <paneerudh@gmail.com>
2025-04-14 16:18:56 +0530

    update

--- 162.Find_peak_elements.py
+++ 162.Find_peak_elements.py
@@ -3,3 +3,17 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] == max(nums):
                 return i
+            
+'''
+optional rust solution:
+impl Solution {
+    pub fn find_peak_element(nums: Vec<i32>) -> i32 {
+        for i in 0..nums.len() {
+            if nums[i] == *nums.iter().max().unwrap() {
+                return i as i32;
+            }
+        }
+        -1
+    }
+}
+'''
