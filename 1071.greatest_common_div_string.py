2531f8cc1b79083f3505b16599203c5f08d9a099
Aneerudh <paneerudh@gmail.com>
2025-04-18 11:39:14 +0530

    update

--- 1071.greatest_common_div_string.py
+++ 1071.greatest_common_div_string.py
@@ -7,3 +7,30 @@ class Solution:
             return candidate
         else:
             return ""
+        
+'''
+rust solution:
+impl Solution {
+    pub fn gcd_of_strings(str1: String, str2: String) -> String {
+        fn gcd(mut a: usize, mut b: usize) -> usize {
+            while b != 0 {
+                let temp = b;
+                b = a % b;
+                a = temp;
+            }
+            a
+        }
+
+        let len1 = str1.len();
+        let len2 = str2.len();
+        let gcd_len = gcd(len1, len2);
+        let candidate = &str1[..gcd_len];
+
+        if candidate.repeat(len1 / gcd_len) == str1 && candidate.repeat(len2 / gcd_len) == str2 {
+            candidate.to_string()
+        } else {
+            "".to_string()
+        }
+    }
+}
+'''
