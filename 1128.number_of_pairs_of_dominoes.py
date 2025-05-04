1b43e1c49342e1854407cd06994ad01f64b84026
Aneerudh <paneerudh@gmail.com>
2025-05-04 22:02:12 +0530

    Added

---
+++ 1128.number_of_pairs_of_dominoes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        count = {}
+        result = 0
+        for a,b in dominoes:
+            key = (min(a,b),max(a,b))
+            if key in count:
+                result += count[key]
+                count[key] += 1
+            else:
+                count[key] = 1
+        return result
