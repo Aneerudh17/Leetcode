a13163062383a1b4dbef1a7e2b8b484750fd55ac
Aneerudh17 <paneerudh@gmail.com>
2025-06-05 22:22:28 +0530

    py

---
+++ 2811.posibbility_to_split.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canSplitArray(self, nums: List[int], m: int) -> bool:
+        n = len(nums)
+
+        if n == 1:
+            return True
+
+        if n == 2:
+            return True
+
+        for i in range(n-1):
+            if nums[i] + nums[i + 1] >= m:
+                return True
+
+        return False
+
