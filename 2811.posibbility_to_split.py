aa682fd7081e8bc961c1b19e657d5c606c475ff2
Aneerudh17 <paneerudh@gmail.com>
2025-06-05 22:22:58 +0530

    js

--- 2811.posibbility_to_split.py
+++ 2811.posibbility_to_split.py
@@ -14,3 +14,28 @@ class Solution:
 
         return False
 
+'''
+js solution:
+/**
+ * @param {number[]} nums
+ * @param {number} m
+ * @return {boolean}
+ */
+var canSplitArray = function(nums, m) {
+    let n = nums.length;
+
+    if (n === 1){
+        return true;
+    }
+
+    if (n === 2){
+        return true;
+    }
+
+    for (let i = 0; i < n-1; i++){
+        if (nums[i] + nums[i + 1] >= m){
+            return true;
+        }
+    }
+    return false;
+'''
