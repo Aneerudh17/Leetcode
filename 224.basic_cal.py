f72e3b94df5600c1f94d5cabec23688804fde18d
Aneerudh17 <paneerudh@gmail.com>
2025-06-07 18:11:47 +0530

    js

--- 224.basic_cal.py
+++ 224.basic_cal.py
@@ -33,3 +33,48 @@ class Solution:
         if num:
             result += sign * num
         return result
+    
+
+'''
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    let stack = [];
+        let result = 0;
+        let num = 0;
+        let sign = 1; // 1 for positive, -1 for negative
+        let i = 0;
+
+        while (i < s.length) {
+            let ch = s[i];
+
+            if (!isNaN(ch) && ch !== ' ') {
+                num = num * 10 + parseInt(ch);
+            } else if (ch === '+' || ch === '-') {
+                result += sign * num;
+                num = 0;
+                sign = ch === '+' ? 1 : -1;
+            } else if (ch === '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (ch === ')') {
+                result += sign * num;
+                num = 0;
+                let prevSign = stack.pop();
+                let prevRes = stack.pop();
+                result = prevRes + prevSign * result;
+            }
+            i++;
+        }
+
+        if (num !== 0) {
+            result += sign * num;
+        }
+
+        return result;
+    }
+'''
