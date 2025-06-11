cbf06659e6c0aa20f214c077b306e8460b24f92c
Aneerudh17 <paneerudh@gmail.com>
2025-06-11 20:46:40 +0530

    js sol

--- 82.remove_duplicates_II.py
+++ 82.remove_duplicates_II.py
@@ -29,3 +29,42 @@ class Solution:
 
         return dummy.next
         
+
+'''
+js solution
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if (!head || !head.next) return head;
+    
+    const dummy = new ListNode(0,head);
+    let prev = dummy;
+    let current = head;
+
+    while (current){
+        let isduplicate = false;
+        while (current.next && current.val == current.next.val){
+            isduplicate = true;
+            current = current.next;
+        }
+        if (isduplicate){
+            prev.next = current.next;
+
+        }
+        else{
+            prev = prev.next
+        }
+        current = current.next
+    }
+    return dummy.next;
+};
+'''
