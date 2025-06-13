182f663b1f5d7c2eb78ec84290568c024840ea7a
Aneerudh17 <paneerudh@gmail.com>
2025-06-13 22:21:11 +0530

    js

--- 1078.occurence_after_bbigram.py
+++ 1078.occurence_after_bbigram.py
@@ -7,3 +7,24 @@ class Solution:
                 result.append(words[i+2])
         return result
        
+'''
+js solution
+/**
+ * @param {string} text
+ * @param {string} first
+ * @param {string} second
+ * @return {string[]}
+ */
+var findOcurrences = function(text, first, second) {
+    const words = text.split(" ");
+    const result = [];
+
+    for (let i = 0; i < words.length - 2; i++) {
+        if (words[i] === first && words[i + 1] === second) {
+            result.push(words[i + 2]);
+        }
+    }
+
+    return result;
+};  
+'''
