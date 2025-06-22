4128a7e6adbc20749c082a67046fffebf5a00787
Aneerudh17 <paneerudh@gmail.com>
2025-06-22 19:46:15 +0530

    js

--- 349.Intersection_of_arrays.py
+++ 349.Intersection_of_arrays.py
@@ -1,3 +1,27 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1)& set(nums2))
+    
+
+'''
+js solution:
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const result = [];
+
+    for (let num of set1) {
+        if (set2.has(num)) {
+            result.push(num);
+        }
+    }
+
+    return result;
+    
+};
+'''
