c11722fe2f0f85490549e42aaa39a11fa50f4770
Aneerudh17 <paneerudh@gmail.com>
2025-07-23 22:06:53 +0530

    py

---
+++ 637.average_of_levels.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        result = []
+        queue = deque([root])
+
+        while queue:
+            size = len(queue)
+            total = 0
+
+            for i in range(size):
+                node = queue.popleft()
+                total += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(total/size)
+        return result
